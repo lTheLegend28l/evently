0040b583a3e224e6c18eda54cc6a8b2361c7bdd5
lTheLegend28l <pradyun.tandra@gmail.com>
2024-02-25 17:33:23 -0600

    fix layout.tsx for deployment

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,7 @@
-export const layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex-center min-h-screen w-full, bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
-      {children}
-    </div>
+    <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">{children}</div>
   );
 };
 
-export default layout;
+export default Layout;
